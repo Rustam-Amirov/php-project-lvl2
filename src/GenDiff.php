2618c58f8d74d08f27d563edf55932282b2d65b0
Rustam Amirov <r.amirov@yahoo.com>
2020-05-29 02:20:38 +0300

    fix bags

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -6,16 +6,16 @@ use function Funct\Collection\flattenAll;
 
 function genDiff($request, $secondPathToFile = false)
 {
-    if (isset($request['<firstFile>']) && file_exists(__DIR__ . '/' . $request['<firstFile>'])) {
-        $firstFile = json_decode(file_get_contents(__DIR__ . '/' . $request['<firstFile>']), true);
-    } elseif ($secondPathToFile && !is_array($request) && file_exists(__DIR__ . '/' . $request)) {
-        $firstFile = json_decode(file_get_contents(__DIR__ . '/' . $request, true));
-    }
-    if (isset($request["<secondFile>"]) && file_exists(__DIR__ . '/' . $request['<secondFile>'])) {
-        $secondFile = json_decode(file_get_contents(__DIR__ . '/' . $request['<secondFile>']), true);
-    } elseif ($secondPathToFile && file_exists(__DIR__ . '/' . $secondPathToFile)) {
-        $secondFile = json_decode(file_get_contents(__DIR__ . '/' . $secondPathToFile), true);
-    }
+	if (file_exists(__DIR__ . '/' . $request['<firstFile>']) && file_exists(__DIR__ . '/' . $request['<secondFile>'])) {
+		$firstFile = json_decode(file_get_contents(__DIR__ . '/' . $request['<firstFile>']), true);
+		$secondFile = json_decode(file_get_contents(__DIR__ . '/' . $request['<secondFile>']), true);
+	} elseif (file_exists(__DIR__ . '/' . $request) && file_exists(__DIR__ . '/' . $secondPathToFile)) {
+		$firstFile = json_decode(file_get_contents(__DIR__ . '/' . $request), true);
+		$secondFile = json_decode(file_get_contents(__DIR__ . '/' . $secondPathToFile), true);
+	} else {
+		echo 'Error';
+	}
+	
     $keys = [array_keys($firstFile), array_keys($secondFile)];
 
     $string = array_reduce(array_unique(flattenAll($keys)), function ($acc, $key) use ($firstFile, $secondFile) {
